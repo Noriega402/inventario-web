001e29839bd3fc01c15d6c02bbe2b78ca0e97a5f
Noriega402 <d.noriegachajon@gmail.com>
2025-11-04 21:50:43 -0600

    Habilitar asignacion masiva

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transfer extends Model
 {
-    //
+    protected $fillable = [
+        'serie',
+        'correlative',
+        'date',
+        'total',
+        'observation',
+        'origin_warehouse_id',
+        'destination_warehouse_id',
+        'reason_id',
+    ];
 }
